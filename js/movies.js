d8d28001ae4309845b49e751abc3c1ba79484eb0
Jake Howard <bsidedrums@Jake-Howards-MacBook-Pro.local>
2018-08-08 18:11:59 -0700

    added

--- js/movies.js
+++ js/movies.js
@@ -11,49 +11,55 @@ $(document).ready(function() {
 		}
 	});
 
+	$('button').on('click', function () {
+		$.ajax({url:"myMovies.json", success: function(result){
+			$('#ajax_test').html(result);
+		}});
+	});
+
 });
 
-myMovies = {
-	"movie": [
-		{
-			"title": "Tears of the Sun",
-			"description": "Navy SEALS",
-			"poster": "img/Tears_of_the_Sun_movie.jpg"
-		},
-		{
-			"title": "John Wick",
-			"description": "Hitman",
-			"poster": "img/john_wick.jpg"
-		},
-		{
-			"title": "John Wick 2",
-			"description": "He is back!",
-			"poster": "img/wick2.jpg"
-		}
+// let xhr = new XMLHttpRequest();
+
+// xhr.onreadystatechange = function () {
+// 	if (xhr.readyState === 4 && xhr.status === 200) {
+// 		myMovies = JSON.parse(xhr.responseText);
 		
-	]
-}
+// 		let $newContent = '';
 
-let xhr = new XMLHttpRequest(); // Create XMLHttpRequest object
+// 		for (var i = 0; i < myMovies.length; i += 1) {
+// 			$newContent += '<div class="poster_container">',
+// 						+= '<img src="' + myMovies.'"'
 
-xhr.onload = function() {
-	if (xhr.status === 200) {
-		responseObject = JSON.parse(xhr.responseText);
+// 		}
+		
+// 	} 
+// }
 
-		let newContent = '';
-		for (var i = 0; i < responseObject.movie.length; i++) {
-			// newContent += '<div class="poster_container">';
-			// newContent += '<img src="' + responseObject.movie[i].poster + ' " />';
-			// newContent += '</div>';
-			newContent += '<div class="summary_container">';
-			newContent += '<h2' + responseObject.movie[i].title + '/>';
-			newContent += '<p' + responseObject.movie[i].description + '/>';
-			newContent += '</div';
-		}
+// xhr.open('GET', 'data/myMovies.json');	
+// xhr.send(); 							
 
-		document.getElementById('one').innerHTML = newContent;
-	}
-};
 
-xhr.open('GET', 'data/data.json', true);	// Prepare request
-// xhr.send(null); 							// Send request
+
+
+// let xhr = new XMLHttpRequest(); // Create XMLHttpRequest object
+
+// xhr.onload = function() {
+// 	if (xhr.status === 200) {
+// 		responseObject = JSON.parse(xhr.responseText);
+
+// 		let newContent = '';
+// 		for (var i = 0; i < responseObject.movie.length; i++) {
+// 			newContent += '<div class="poster_container">';
+// 			newContent += '<img src="' + responseObject.movie[i].poster + ' " />';
+// 			newContent += '</div>';
+// 			newContent += '<div class="summary_container">';
+// 			newContent += '<h2' + responseObject.movie[i].title + '/>';
+// 			newContent += '<p' + responseObject.movie[i].description + '/>';
+// 			newContent += '</div';
+// 		}
+
+// 		document.getElementById('one').innerHTML = newContent;
+// 	}
+// };
+
